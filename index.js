7bd92de611ef9d8a2c6dcf9881e6c16644fade3d
Revan Sarbast <revansarbast8@gmail.com>
2020-07-01 07:56:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ function  mapToNegativize(sourceArray){
  for(let i = 0; i < sourceArray.length; i++){
    newArr[i] = sourceArray[i]*-1;
  }
+<<<<<<< HEAD
  
  return newArr
 }
 console.log(mapToNegativize([1, 2, 3, -9]));
+=======
+ return newArr
+}
+console.log(mapToNegativize([1,5,3,77,1]));
+>>>>>>> 296a02b2f6bc98f169825a5c73851bc8060731e8
 function mapToNoChange(sourceArray){
   return sourceArray
 }
 function mapToDouble(sourceArray){
   let newArr= [];
   for(let i = 0; i < sourceArray.length; i++){
+<<<<<<< HEAD
     newArr[i] = sourceArray[i]*2;
   }
   return newArr
@@ -46,3 +53,9 @@ function reduceToAnyTrue(sourceArray){
   return !!startingPoint
 }
 console.log(reduceToAllTrue([false,true,true]));
+=======
+    newArr[i] = Double(sourceArray[i]);
+  }
+  return newArr
+}
+>>>>>>> 296a02b2f6bc98f169825a5c73851bc8060731e8
